8a7edf563f59e9a58d86c4fde341bda376cff0db
Mohamed14-7 <96206856+Mohamed14-7@users.noreply.github.com>
2025-03-18 14:30:35 +0200

    first vedio

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('admin/products/create', function () {
+    return view('admin.products.create');
+});
+Route::get('admin/products', function () {
+    return view('admin.products.index');
+});
+
